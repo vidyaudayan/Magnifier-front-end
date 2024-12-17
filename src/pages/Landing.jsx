727daa859ecac58da9164ba45f844289b2be2d9b
vidyaudayan <vidyaudayan89@gmail.com>
2024-12-17 12:43:29 +0530

    redux 1

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -104,7 +104,7 @@ export const LandingPage = () => {
             dispatch(
                 updateMetrics({
                     walletAmount,
-                    userReactions,
+                   totalLikes,totalDislikes,
                     postCount,
                 })
             );
